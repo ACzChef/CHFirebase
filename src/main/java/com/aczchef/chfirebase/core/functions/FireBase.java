6e447dd8b9beb2a725d3c97fd22ca1192e4a3c63
Chris Gallarno <cgallarno200@gnail.com>
2013-10-01 21:05:27 -0400

    Finished adding callbacks Added fire_read

--- src/main/java/com/aczchef/chfirebase/core/functions/FireBase.java
+++ src/main/java/com/aczchef/chfirebase/core/functions/FireBase.java
@@ -1,24 +1,29 @@
 package com.aczchef.chfirebase.core.functions;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.laytonsmith.PureUtilities.StackTraceUtils;
 import com.laytonsmith.PureUtilities.Version;
-import com.laytonsmith.abstraction.Implementation;
 import com.laytonsmith.abstraction.StaticLayer;
 import com.laytonsmith.annotations.api;
 import com.laytonsmith.core.CHLog;
+import com.laytonsmith.core.CHVersion;
+import com.laytonsmith.core.LogLevel;
 import com.laytonsmith.core.constructs.CClosure;
+import com.laytonsmith.core.constructs.CString;
 import com.laytonsmith.core.constructs.CVoid;
 import com.laytonsmith.core.constructs.Construct;
 import com.laytonsmith.core.constructs.Target;
 import com.laytonsmith.core.environments.Environment;
 import com.laytonsmith.core.environments.GlobalEnv;
 import com.laytonsmith.core.exceptions.ConfigRuntimeException;
+import com.laytonsmith.core.exceptions.FunctionReturnException;
+import com.laytonsmith.core.exceptions.ProgramFlowManipulationException;
 import com.laytonsmith.core.functions.AbstractFunction;
 import com.laytonsmith.core.functions.Exceptions;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import com.laytonsmith.core.functions.Exceptions.ExceptionType;
 
 /**
  *
@@ -27,17 +32,8 @@ import java.util.concurrent.ThreadFactory;
 public class FireBase {
     
     @api
-    public static class base_set_value extends AbstractFunction {
+    public static class fire_set extends AbstractFunction {
 	
-	private static int threadCount = 0;
-	    
-	private static final ExecutorService threadPool = Executors.newFixedThreadPool(3, new ThreadFactory() {
-
-		public Thread newThread(Runnable r) {
-		    return new Thread(r, Implementation.GetServerType().getBranding() + "-firebase-write-" + (threadCount++));
-		}
-	   });
-
         public Exceptions.ExceptionType[] thrown() {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
@@ -50,7 +46,7 @@ public class FireBase {
             return false;
         }
 
-        public Construct exec(final Target t, Environment environment, Construct... args) throws ConfigRuntimeException {
+        public Construct exec(final Target t, final Environment environment, Construct... args) throws ConfigRuntimeException {
             
 	    Firebase link = new Firebase(args[0].val());
             final Firebase ref;
@@ -79,37 +75,73 @@ public class FireBase {
                     callback = null;
                     value = args[1].val();
                     break;
-                //should never run
+                //Should never run
                 default: value = "";
                     callback = null;
             }
             
             ref = link.child(childern);
-            
-            environment.getEnv(GlobalEnv.class).GetDaemonManager().activateThread(null);
-	    threadPool.submit(new Runnable() {
+	    
+	    final Runnable execute = new Runnable() {
 		public void run() {
 		    ref.setValue(value, new Firebase.CompletionListener() {
 
 			public void onComplete(FirebaseError fe) {
+			    final String error;
                             if (fe != null) {
-                                CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, fe.getMessage(), t);
-                            }
+                                error = fe.getMessage();
+                            } else {
+				error = "";
+			    }
+			    
+			    final CString CError = new CString(error, t);
+			    StaticLayer.GetConvertor().runOnMainThreadLater(environment.getEnv(GlobalEnv.class).GetDaemonManager(), new Runnable() {
+				public void run() {
+				    try{
+					callback.execute(new Construct[]{CError});
+				    } catch(FunctionReturnException e){
+					//Just ignore this if it's returning void. Otherwise, warn.
+					//TODO: Eventually, this should be taggable as a compile error
+					if(!(e.getReturn() instanceof CVoid)){
+					    CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.WARNING, "Returning a value from the closure. The value is"
+					    + " being ignored.", t);
+					}
+				    } catch(ProgramFlowManipulationException e){
+					//This is an error
+					CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.WARNING, "Only return may be used inside the closure.", t);
+				    } catch(ConfigRuntimeException e){
+					ConfigRuntimeException.React(e, environment);
+				    } catch(Throwable e){
+					//Other throwables we just need to report
+					CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.ERROR, "An unexpected exception has occurred. No extra"
+					+ " information is available, but please report this error:\n" + StackTraceUtils.GetStacktrace(e), t);
+				    }
+				}
+			    });
 			}
 		    });
 		}
-	    });
-            
-            
-            
+	    };
+	    
+	    if (callback == null) {
+		ref.setValue(value);
+	    } else {
+		Thread th = new Thread() {
+		    @Override
+		    public void run() {
+			execute.run();
+		    }
+		};
+		
+		th.start();
+	    }
 	    
             return new CVoid(t);
-	    
 	    
         }
 
         public String getName() {
-            return "base_set_value";
+            return "fire_set";
         }
 
         public Integer[] numArgs() {
@@ -121,8 +153,114 @@ public class FireBase {
         }
 
         public Version since() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            return CHVersion.V3_3_1;
         }
     
+    }
+    
+    @api
+    public static class fire_read extends AbstractFunction {
+
+	public ExceptionType[] thrown() {
+	    throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	public boolean isRestricted() {
+	    return true;
+	}
+
+	public Boolean runAsync() {
+	    return false;
+	}
+
+	public Construct exec(final Target t, final Environment environment, Construct... args) throws ConfigRuntimeException {
+	    
+	    Firebase link = new Firebase(args[0].val());
+            final Firebase ref;
+            String childern = "";
+            final CClosure callback;
+	    
+	    switch(args.length) {
+		case 3:
+		    childern = args[1].val();
+		    callback = (CClosure) args[2];
+		    break;
+		case 2:
+		    callback = (CClosure) args[1];
+		    break;
+		//Should never run
+		default:
+		    callback = null;
+	    }
+	    
+	    ref = link.child(childern);
+	    
+	    final Runnable execute = new Runnable() {
+		public void run() {
+		    ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+			public void onDataChange(DataSnapshot ds) {
+			    final CString data = new CString(ds.getValue().toString(), t);
+			    
+			    StaticLayer.GetConvertor().runOnMainThreadLater(environment.getEnv(GlobalEnv.class).GetDaemonManager(), new Runnable() {
+				public void run() {
+				    try{
+					callback.execute(new Construct[]{data});
+				    } catch(FunctionReturnException e){
+					//Just ignore this if it's returning void. Otherwise, warn.
+					//TODO: Eventually, this should be taggable as a compile error
+					if(!(e.getReturn() instanceof CVoid)){
+					    CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.WARNING, "Returning a value from the closure. The value is"
+					    + " being ignored.", t);
+					}
+				    } catch(ProgramFlowManipulationException e){
+					//This is an error
+					CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.WARNING, "Only return may be used inside the closure.", t);
+				    } catch(ConfigRuntimeException e){
+					ConfigRuntimeException.React(e, environment);
+				    } catch(Throwable e){
+					//Other throwables we just need to report
+					CHLog.GetLogger().Log(CHLog.Tags.RUNTIME, LogLevel.ERROR, "An unexpected exception has occurred. No extra"
+					+ " information is available, but please report this error:\n" + StackTraceUtils.GetStacktrace(e), t);
+				    }
+				}
+			    });
+			}
+
+			public void onCancelled() {
+			    throw new UnsupportedOperationException("Not supported yet.");
+			}
+		    });
+		}
+	    };
+	    
+	    Thread th = new Thread() {
+		@Override
+		public void run() {
+		    execute.run();
+		}
+	    };
+	    th.start();
+	    
+	    return new CVoid(t);
+	    
+	}
+
+	public String getName() {
+	    return "fire_read";
+	}
+
+	public Integer[] numArgs() {
+	    return new Integer[] {2, 3};
+	}
+
+	public String docs() {
+	    return "";
+	}
+
+	public Version since() {
+	    return CHVersion.V3_3_1;
+	}
+	
     }
 }
